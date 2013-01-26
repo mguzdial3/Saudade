90dcad7f65d52d013990a1da9241241bc323f4a2
Matthew Guzdial <mguzdial3@gatech.edu>
2013-01-25 23:16:34 -0500

    Fixing face

--- Assets/Scripts/FacePlayah.cs
+++ Assets/Scripts/FacePlayah.cs
@@ -1,32 +1,4 @@
-<<<<<<< HEAD:Assets/Scripts/FacePlayah.cs
-using UnityEngine;
-using System.Collections;
 
-public class FacePlayah : MonoBehaviour {
-	GameObject player;
-	
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		Vector3 face = player.transform.position;
-		face.y=transform.position.y;
-		transform.LookAt(face);
-		
-		Vector3 distance = player.transform.position-transform.position;
-		
-		distance.Normalize();
-		
-		transform.position+=distance*0.01f;
-			
-	
-	}
-}
-=======
 using UnityEngine;
 using System.Collections;
 
@@ -44,4 +16,3 @@ public class FacePlayah : MonoBehaviour {
 	
 	}
 }
->>>>>>> Retrying Commit?:Assets/FacePlayah.cs
